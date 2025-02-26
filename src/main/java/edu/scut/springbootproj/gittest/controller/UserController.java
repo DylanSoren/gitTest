0ab3dacce99ad01f5a096f7d2effddf9aa6271da
YSQ <shaoqianyi7@163.com>
2025-02-26 20:48:38 +0800

    add bean

--- src/main/java/edu/scut/springbootproj/gittest/controller/UserController.java
+++ src/main/java/edu/scut/springbootproj/gittest/controller/UserController.java
@@ -1,8 +1,12 @@
 package edu.scut.springbootproj.gittest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author DylanS
  * @version 1.0
  */
+@RestController
 public class UserController {
 }
